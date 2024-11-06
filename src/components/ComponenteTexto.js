973998bb4bccfa1b1e73805147fc94007ff26d0c
renan <renan.br3@gmail.coml>
2024-11-05 22:03:28 -0300

    Componentes Export e Import

---
+++ src/components/ComponenteTexto.js
@@ -0,0 +1,9 @@
+function ComponenteTexto() {
+    return (
+        <div>
+            <p>Este é o texto em parágrafo do componente texto do React.</p>
+        </div>
+    )
+}
+
+export default ComponenteTexto;
